7e1b9e966c4f6fa1134a61d7313030faad4aff11
guiofranca <dankan282@gmail.com>
2023-02-27 20:38:19 -0300

    TableName Tests

---
+++ tests/Template.Data.Tests/Repositories/Shared/TableNameTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Data.Repositories.Shared;
+
+namespace Template.Data.Tests.Repositories.Shared;
+
+public class TableNameTests
+{
+    [Fact]
+    public void Of_ReturnsTheClassNameInLowercaseSuffixedWithS()
+    {
+        var expected = "TableNameTestss".ToLower();
+        var actual = TableName.Of<TableNameTests>();
+        actual.Should().Be(expected);
+    }
+}
